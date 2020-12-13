87f10726a48ba513d3099c5f27d1683085b96778
giegeralexandra <giegeralexandra@gmail.com>
2020-12-13 13:01:15 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_song_view 
+    song.artist.name + " - " + song.title
+  end
+
+  def artist_name
+    self.artist.name 
+  end
+
+
 end
